4eb6c24ee148c5966e7c89adbbdf53e6faa71fdd
MrVaylis <mrvaylis@icloud.com>
2024-10-12 21:11:03 +0300

    03.03.2022

---
+++ src/main/java/ru/naulbimix/nlbWGPatch/Main.java
@@ -0,0 +1,16 @@
+package ru.naulbimix.nlbWGPatch;
+
+import org.bukkit.plugin.java.*;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Enabled nlbWGPatch by NaulbiMIX | Leaked by FlatiCommunity VIP Resources (t.me/flatishop_bot)");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Disabled nlbWGPatch by NaulbiMIX | Leaked by FlatiCommunity VIP Resources (t.me/flatishop_bot)");
+    }
+}
